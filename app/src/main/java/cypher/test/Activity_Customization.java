21864d4fb16f60b9b83d0c40592ed9bcbfae0461
SurajGoel <sgoel01@gmail.com>
2016-08-09 22:37:16 +0530

    Update Activity_Customization.java

--- app/src/main/java/cypher/test/Activity_Customization.java
+++ app/src/main/java/cypher/test/Activity_Customization.java
@@ -3,6 +3,7 @@ package cypher.test;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
@@ -13,14 +14,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+
 public class Activity_Customization extends Activity implements View.OnClickListener {
 
 
     private LinearLayout apn,host,bid,device,cyc,reset;
     private RelativeLayout cal;
     private TextView tv_device_name;
-    private String match_device = "\n\nEnter the new Device Name";
-    private String confirm = "\nDone\n";
+    private String match_device = "Enter new Device Name",match_result = "Done",setString="";
+    private BlueController blueController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +43,7 @@ public class Activity_Customization extends Activity implements View.OnClickList
         device.setOnClickListener(this);
         cal.setOnClickListener(this);
         cyc.setOnClickListener(this);
+        blueController = (BlueController)this.getApplicationContext();
     }
 
     //back to the logs page
@@ -52,58 +56,32 @@ public class Activity_Customization extends Activity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.device:
-                Activity_Connect.ActionStatic("Device Name");
-                // VERY BAD PRACTICE, NEED TO IMPLEMENT A THREAD METHOD FOR THIS
-                try {
-                    Thread.sleep(500);
-                } catch (Exception e) {
-
-                }
-                String new_command = Activity_Connect.ProvideString();
-                if(new_command.equals(match_device)) {
-                    ShowDialog(device);
-                }
-                else Toast.makeText(this, "Could not convey message correctly", Toast.LENGTH_SHORT).show();
+                Write("Device Name");
+                new Reader().execute();
                 break;
         }
     }
 
-    public boolean ShowDialog(View v) {
-        switch (v.getId()) {
+    private void ShowDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        final EditText editText = new EditText(this);
+        alert.setMessage("Please Set the Device Name");
+        alert.setTitle("Note");
+        alert.setView(editText);
 
-            case R.id.device:
-                AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                final EditText editText = new EditText(this);
-                alert.setMessage("Please Set the Device Name");
-                alert.setTitle("Note");
-                alert.setView(editText);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                Write(setString=editText.getText().toString());
+                new Reader().execute();
+            }
+        });
 
-                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        TextView text = (TextView) device.findViewById(R.id.tv_device_name);
-                        text.setText(editText.getText().toString());
-                        Activity_Connect.ActionStatic(editText.getText().toString());
-                        // VERY BAD PRACTICE, NEED TO IMPLEMENT A THREAD METHOD FOR THIS
-                        try {
-                            Thread.sleep(500);
-                        } catch (Exception e) {
-
-                        }
-                        String new_command = Activity_Connect.ProvideString(),temp="";
-                        if(new_command.equals(confirm)) temp = "Successful";
-                        else temp = "UnSuccessful";
-                        Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        // what ever you want to do with No option.
-                    }
-                });
-                alert.show();
-        }
-        return true;
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // what ever you want to do with No option.
+            }
+        });
+        alert.show();
     }
 
     //disconnecting the connected bluetooth device
@@ -113,5 +91,42 @@ public class Activity_Customization extends Activity implements View.OnClickList
         startActivity(intent);
         Toast.makeText(this, "Device disconnected", Toast.LENGTH_SHORT).show();
         finish();
+    }
+
+    private class Reader extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void o) {
+            doAction();
+            super.onPostExecute(o);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            while (blueController.activityLock) {
+                continue;
+            }
+            return null;
+        }
+    }
+
+    private void doAction() {
+        if(blueController.command.contains(match_device)) {
+            blueController.command = "";
+            ShowDialog();
+        }
+        else if(blueController.command.contains(match_result)) {
+            blueController.command = "";
+            Toast.makeText(this, "Successfull", Toast.LENGTH_SHORT).show();
+            tv_device_name.setText(setString);
+        }
+    }
+
+    private void Write(String message) {
+        blueController.writeMessage(message);
     }
 }
