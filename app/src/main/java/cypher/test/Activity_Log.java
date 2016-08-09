19da778138485171476f63dd3aa233ae893ec829
SurajGoel <sgoel01@gmail.com>
2016-08-09 22:37:42 +0530

    Update Activity_Log.java

--- app/src/main/java/cypher/test/Activity_Log.java
+++ app/src/main/java/cypher/test/Activity_Log.java
@@ -2,28 +2,45 @@ package cypher.test;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class Activity_Log extends Activity {
-
+    private boolean  isThreadRunning = false,lockDown=false;
+    private TextView textShow;
+    private EditText textedit;
+    private String match_1 = "C U S T O M   M O D E";
+    private BlueController blueController;
+    private  BluetoothSerial bluetoothSerialthis;
+    private Button btn_customization,btn_calibration,btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_log);
-        Intent intent;
+        blueController = (BlueController) this.getApplicationContext();
+        btn_calibration = (Button) findViewById(R.id.btn_calibration);
+        btn_customization = (Button)  findViewById(R.id.btn_customization);
     }
 
     //taking to calibration page
     public void Calibration(View view) {
-        doAction("cal");
+        nonClickable();
     }
+
 
     //taking to the parameters page
     public void Customization(View view) {
-        doAction("cust");
+        Write("Customize");
+        nonClickable();
+        new Reader().execute();
+        //Toast.makeText(this, blueController.command, Toast.LENGTH_LONG).show();
     }
 
     //disconnecting the connected bluetooth device
@@ -35,10 +52,45 @@ public class Activity_Log extends Activity {
         finish();
     }
 
-    private void doAction(String id) {
-        Intent intent = new Intent(Activity_Log.this, Activity_Connect.class);
-        intent.putExtra("button_id", id);
-        setResult(Activity.RESULT_OK,intent);
+
+    private void Write(String message) {
+        blueController.writeMessage(message);
+    }
+
+    private class Reader extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void o) {
+            super.onPostExecute(o);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            while (blueController.activityLock) {
+                continue;
+            }
+            doAction();
+            return null;
+        }
+    }
+
+    private void doAction() {
+        String temp = blueController.command;
+        Log.d("messg", blueController.command);
+        if(blueController.command.contains(match_1)) {
+            blueController.command = "";
+            Intent intent = new Intent(this, Activity_Customization.class);
+            startActivity(intent);
+        }
         finish();
+    }
+
+    private void nonClickable() {
+        btn_calibration.setClickable(false);
+        btn_customization.setClickable(false);
     }
 }
