b75811a528d43123fac3c96a23a5a3f9b1d0fe36
SurajGoel <sgoel01@gmail.com>
2016-08-09 22:36:44 +0530

    Update Activity_Connect.java

--- app/src/main/java/cypher/test/Activity_Connect.java
+++ app/src/main/java/cypher/test/Activity_Connect.java
@@ -7,26 +7,28 @@ import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Parcelable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Activity_Connect extends AppCompatActivity
-        implements BluetoothSerialListener, BluetoothDeviceListDialog.OnDeviceSelectedListener, View.OnClickListener,View.OnLongClickListener{
+public class Activity_Connect extends Activity
+        implements BluetoothDeviceListDialog.OnDeviceSelectedListener, View.OnClickListener,View.OnLongClickListener{
 
     private static final int REQUEST_ENABLE_BLUETOOTH = 1;
+    private EditText editText;
+    private BluetoothSerial bluetoothSerial;
+    private final String match_1 = "Device Name";
+    private static boolean  isThreadRunning = false;
+    private String command="";
 
-    private static BluetoothSerial bluetoothSerial;
-
-    private ScrollView svTerminal;
     private TextView text_connect,Testing;
-
-    private boolean crlf = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +36,10 @@ public class Activity_Connect extends AppCompatActivity
         setContentView(R.layout.layout_connect);
         text_connect = (TextView) findViewById(R.id.text_connect);
         Testing = (TextView) findViewById(R.id.Testing);
-        bluetoothSerial = new BluetoothSerial(this, this);
+        editText = (EditText) findViewById(R.id.editText);
+        bluetoothSerial = ((BlueController)this.getApplicationContext()).bluetoothSerial;
     }
 
-    public static void ActionStatic(String text) {
-        bluetoothSerial.write(text);
-    }
-
-    public static String ProvideString() {
-        return command;
-    }
     //handling connection call
     public void Connect(View view) {
         showDeviceListDialog();
@@ -56,7 +52,7 @@ public class Activity_Connect extends AppCompatActivity
         super.onStart();
 
         // Check Bluetooth availability on the device and set up the Bluetooth adapter
-        bluetoothSerial.setup();
+//        bluetoothSerial.setup();
     }
 
     @Override
@@ -64,11 +60,11 @@ public class Activity_Connect extends AppCompatActivity
         super.onResume();
 
         // Open a Bluetooth serial port and get ready to establish a connection
-        if (bluetoothSerial.checkBluetooth() && bluetoothSerial.isBluetoothEnabled()) {
+       /* if (bluetoothSerial.checkBluetooth() && bluetoothSerial.isBluetoothEnabled()) {
             if (!bluetoothSerial.isConnected()) {
-                bluetoothSerial.start();
+               // bluetoothSerial.start();
             }
-        }
+        }*/
     }
 
     @Override
@@ -76,7 +72,7 @@ public class Activity_Connect extends AppCompatActivity
         super.onStop();
 
         // Disconnect from the remote device and close the serial port
-        bluetoothSerial.stop();
+       // bluetoothSerial.stop();
     }
 
 
@@ -112,19 +108,113 @@ public class Activity_Connect extends AppCompatActivity
                         break;
                     case "cust":
                         bluetoothSerial.write("Customize");
+                        while (isThreadRunning) {
+                            continue;
+                        }
+                        Toast.makeText(this, "Hello there ",Toast.LENGTH_LONG).show();
                         text_connect.setText("Sending for Customization");
-                        //startActivity(intent);
+                        if(command.contains(match_1)) {
+                            startActivity(intent);
+                        }
+                        else text_connect.setText(command);
+                        command="";
                         break;
                     case "cancel":
-
                         break;
                 }
                 break;
             case 3:
-
-
                 break;
         }
+    }
+
+    public void sendMessage(View v) {
+        bluetoothSerial.write(editText.getText().toString());
+        editText.setText("");
+    }
+
+
+    /* Implementation of BluetoothSerialListener */
+
+
+
+    /*@Override
+    public void onConnectingBluetoothDevice() {
+        updateBluetoothState();
+    }
+
+    @Override
+    public void onBluetoothDeviceConnected(String name, String address) {
+        invalidateOptionsMenu();
+        updateBluetoothState();
+        ImageButton btn = (ImageButton) findViewById(R.id.blue_btn);
+        btn.setClickable(false);
+        btn.setVisibility(View.INVISIBLE);
+        bluetoothSerial.write("Hello There Raspberry");
+        Activity_Log.GetBluetoothSerial(bluetoothSerial);
+      //  Parcelable log_class = Parcels.wrap(bluetoothSerial);
+       // Bundle b = new Bundle();
+        //b.putParcelable("forlog", log_class);
+        Intent intent = new Intent(Activity_Connect.this, Activity_Log.class);
+        //intent.putExtras(b);
+        startActivity(intent);
+        //finish();
+    }
+
+
+    @Override
+    public void onBluetoothSerialRead(String message) {
+        // Print the incoming message on the terminal screen
+      /*  tvTerminal.append(getString(R.string.terminal_message_template,
+                bluetoothSerial.getConnectedDeviceName(),
+                message));
+        command+=message;
+       // if(!isThreadRunning) count.start();
+    }
+
+    @Override
+    public void onBluetoothSerialWrite(String message) {
+        // Print the outgoing message on the terminal screen;
+        //svTerminal.post(scrollTerminalToBottom);
+    }*/
+
+    /* Implementation of BluetoothDeviceListDialog.OnDeviceSelectedListener */
+
+    @Override
+    public void onBluetoothDeviceSelected(BluetoothDevice device) {
+        // Connect to the selected remote Bluetooth device
+        bluetoothSerial.connect(device);
+        Intent intent = new Intent(this, Activity_Log.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /* End of the implementation of listeners */
+
+   /* @Override
+    public void onBluetoothNotSupported() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.no_bluetooth)
+                .setPositiveButton(R.string.action_quit, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    @Override
+    public void onBluetoothDisabled() {
+        Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBluetooth, REQUEST_ENABLE_BLUETOOTH);
+    }
+
+    @Override
+    public void onBluetoothDeviceDisconnected() {
+        invalidateOptionsMenu();
+        updateBluetoothState();
     }
 
     private void updateBluetoothState() {
@@ -152,7 +242,7 @@ public class Activity_Connect extends AppCompatActivity
         if (getSupportActionBar() != null) {
             getSupportActionBar().setSubtitle(subtitle);
         }
-    }
+    }*/
 
     private void showDeviceListDialog() {
         // Display dialog for selecting a remote Bluetooth device
@@ -164,152 +254,9 @@ public class Activity_Connect extends AppCompatActivity
         dialog.show();
     }
 
-    /* Implementation of BluetoothSerialListener */
-
     @Override
-    public void onBluetoothNotSupported() {
-        new AlertDialog.Builder(this)
-                .setMessage(R.string.no_bluetooth)
-                .setPositiveButton(R.string.action_quit, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                })
-                .setCancelable(false)
-                .show();
+    protected void onDestroy() {
+        super.onDestroy();
+        //bluetoothSerial.stop();
     }
-
-    @Override
-    public void onBluetoothDisabled() {
-        Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        startActivityForResult(enableBluetooth, REQUEST_ENABLE_BLUETOOTH);
-    }
-
-    @Override
-    public void onBluetoothDeviceDisconnected() {
-        invalidateOptionsMenu();
-        updateBluetoothState();
-    }
-
-    @Override
-    public void onConnectingBluetoothDevice() {
-        updateBluetoothState();
-    }
-
-    @Override
-    public void onBluetoothDeviceConnected(String name, String address) {
-        invalidateOptionsMenu();
-        updateBluetoothState();
-        ImageButton btn = (ImageButton) findViewById(R.id.blue_btn);
-        btn.setClickable(false);
-        btn.setVisibility(View.INVISIBLE);
-        Intent intent = new Intent(this, Activity_Customization.class);
-        startActivity(intent);
-       /* Intent intent = new Intent(Activity_Connect.this, Activity_Log.class);
-        Bundle b = new Bundle();
-        intent.putExtra("blueObject", bluetoothSerial);
-        startActivityForResult(intent, 2);*/
-    }
-
-    private boolean isFirstMess=true;
-    private int commandNumber = 1;
-    private long currTime,timeout=0;
-    private boolean isThreadRunning = false;
-    private static String command="";
-    @Override
-    public void onBluetoothSerialRead(String message) {
-        // Print the incoming message on the terminal screen
-      /*  tvTerminal.append(getString(R.string.terminal_message_template,
-                bluetoothSerial.getConnectedDeviceName(),
-                message));*/
-        timeout=System.currentTimeMillis()+250;
-        command+=message;
-        if(isThreadRunning == false) countDown.start();
-
-       /* char[] mess = message.toCharArray();
-        for(int index=0 ; index<mess.length ; index++) {
-            if(Character.getNumericValue(mess[index]) == commandNumber) {
-                addButtontoGUI(command);
-                commandNumber++;
-                command="";
-            }
-            else command+=mess[index];
-        }
-        svTerminal.post(scrollTerminalToBottom);*/
-    }
-
-    @Override
-    public void onBluetoothSerialWrite(String message) {
-        // Print the outgoing message on the terminal screen;
-        //svTerminal.post(scrollTerminalToBottom);
-    }
-
-    /* Implementation of BluetoothDeviceListDialog.OnDeviceSelectedListener */
-
-    @Override
-    public void onBluetoothDeviceSelected(BluetoothDevice device) {
-        // Connect to the selected remote Bluetooth device
-        bluetoothSerial.connect(device);
-    }
-
-    /* End of the implementation of listeners */
-
-    private final Runnable scrollTerminalToBottom = new Runnable() {
-        @Override
-        public void run() {
-            // Scroll the terminal screen to the bottom
-            svTerminal.fullScroll(ScrollView.FOCUS_DOWN);
-        }
-    };
-
-    private final Thread countDown = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            isThreadRunning=true;
-            while (System.currentTimeMillis()<timeout) {
-            }
-            isThreadRunning = false;
-            //addtoGUI();
-            //ProvideString();
-        }
-    });
-
-    private String match = "\n\n\t C U S T O M    M O D E \n\n";
-    private void addtoGUI() {
-        String temp = command;
-        command="";
-        Testing.setText(temp);
-        String test = temp.substring(0,30);
-        if(test.equals(match)) {
-            Intent intent = new Intent(this,Activity_Customization.class);
-            startActivity(intent);
-        }
-        /*char[] array = temp.toCharArray();
-        boolean onePassed=false;
-        String line="";
-        for(int i=0 ; i<array.length ; i++) {
-
-            if(array[i] == '\n') {
-                if(onePassed==false) onePassed=true;
-                else {
-                    addButtonToGUI(line);
-                    line="";
-                }
-                continue;
-            }
-            line+=array[i];
-        }*/
-    }
-
-    private int idNumber=0;
-    private void addButtonToGUI(String line) {
-        Button dynamicBtn = new Button(this);
-        dynamicBtn.getLayoutParams().width = LayoutParams.WRAP_CONTENT;
-        dynamicBtn.getLayoutParams().height = LayoutParams.WRAP_CONTENT;
-        dynamicBtn.setId(idNumber);
-        dynamicBtn.setOnClickListener(this);
-    }
-
-
 }
